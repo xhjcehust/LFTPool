e8fd97a819873e946e7ee86183f14a4b626d8205
xiaohengjin <xiaohengjin@youzan.com>
2017-08-27 23:41:20 +0800

    format code

--- tpool.h
+++ tpool.h
@@ -2,8 +2,8 @@
 #define __TPOOL_H__
 
 enum schedule_type {
-	ROUND_ROBIN,
-	LEAST_LOAD
+    ROUND_ROBIN,
+    LEAST_LOAD
 };
 
 void *tpool_init(int num_worker_threads);
@@ -13,9 +13,9 @@ int tpool_inc_threads(void *pool, int num_inc);
 void tpool_dec_threads(void *pool, int num_dec);
 
 int tpool_add_work(void *pool, void(*routine)(void *), void *arg);
-/* 
+/*
 @finish:  1, complete remaining works before return
-		0, drop remaining works and return directly
+        0, drop remaining works and return directly
 */
 void tpool_destroy(void *pool, int finish);
 
